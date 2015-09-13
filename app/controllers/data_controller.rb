2f7f489d4b4dc5f8942674495962bcd1c57794c8
Kameron Robinson <Ka12@umbc.edu>
2015-09-13 04:59:30 +0000

    first undescriptive commit message

--- app/controllers/data_controller.rb
+++ app/controllers/data_controller.rb
@@ -1,37 +1,44 @@
 class DataController < ApplicationController
- def get_catalog
-   # include HTTParty
-   ## @results = HTTParty.get("https://rubygems.org/api/v1/versions/httparty.json")
-   require 'json'
-   value = '{
-  "request": {
-    "passengers": {
-      "adultCount": 1
-    },
-    "slice": [
-      {
-        "origin": "BOS",
-        "destination": "LAX",
-        "date": "YYYY-MM-DD"
-      },
-      {
-        "origin": "LAX",
-        "destination": "BOS",
-        "date": "YYYY-MM-DD"
-      }
-    ]
-  }
-}'
-   parsed = JSON.parse(value)
-   @results = HTTParty.post("https://www.googleapis.com/qpxExpress/v1/trips/search?key=AIzaSyDh-1ynhECeZsfhlXwi6Eqwh8HjUCrIDXM", parsed)
-   #binding.pry
-   #any instance in action is immediately given to view
-  end
+#  PLEASE DON'T MODIFY, THIS WORKS PRETTY WELL SO FAR, ONLY THING THAT DOESN'T WORK IS JSON PARSING
+#    require 'json'
+#   require 'pp'
+#   class Party
+#     include HTTParty
+#   end
+#   def get_catalog
+#  value = {
+#   request: {
+#     passengers: {
+#       adultCount: '1'
+#     },
+#     slice: [
+#       {
+#         origin: 'BOS',
+#         destination: 'LAX',
+#         date: '2015-12-21'
+#       },
+#       {
+#         origin: 'LAX',
+#         destination: 'BOS',
+#         date: '2015-12-21'
+#       }
+#     ]
+#   }
+# }
+# #     parsed = JSON.parse(value)
+#     @results = pp Party.post("https://www.googleapis.com/qpxExpress/v1/trips/search?key=AIzaSyCtPUW5gzsCX6mgMLCCFSU0bmFFvjl0B2k", value)
+#    #any instance in action is immediately given to view
+#   end
+  #   AIzaSyDh-1ynhECeZsfhlXwi6Eqwh8HjUCrIDXM <- Old,nonfunctioning Api key
 # @response = HTTParty.get("http://rubygems.org/api/v1/versions/httparty.json")
 # @result = @response.body
 # respond_to do |format|
 #    format.json { render :json => JSON.parse(@result) }
 #    format.html { render "index.html.erb" }
 #   end
-# curl -d @request.json --header "Content-Type: application/json" https://www.googleapis.com/qpxExpress/v1/trips/search?key=AIzaSyDh-1ynhECeZsfhlXwi6Eqwh8HjUCrIDXM
+
+  def get_catalog
+    @results = HTTParty.get("http://rubygems.org/api/v1/versions/httparty.json")
+  end
+
 end
