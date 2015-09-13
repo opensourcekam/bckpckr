aa7ee71e9582836aad1b7ab2554fa2536b847320
Kameron Robinson <Ka12@umbc.edu>
2015-09-13 09:37:13 +0000

    make it better

--- app/controllers/data_controller.rb
+++ app/controllers/data_controller.rb
@@ -6,12 +6,12 @@ class DataController < ApplicationController
   end
 
   def new
-#     :entry = Entry.new
+    :entry = Entry.new
   end
 
   def update
   end
-  
+
 
 #   def createurls
 #   @url = "https://api.test.sabre.com/v2/shop/flights/fares?origin=" + ABB + "&destination=" + ABB + "&lengthofstay=" + NUM +  (optional + "&departuredate=" + YYYY-MM-DD) + "&pointofsalecountry=US"
